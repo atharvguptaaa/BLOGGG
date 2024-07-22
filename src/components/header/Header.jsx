f5fc8dc3d4181500be7c9924889b42e96cc45423
atharvguptaaa <workinatharv@gmail.com>
2024-07-22 17:39:25 +0530

    c

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,72 +1,59 @@
-import React from 'react'
-import {Container,Logo,LogoutBtn} from '../index'
-import { Link } from 'react-router-dom'
-import { useSelector } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
+import React, { useState } from 'react';
+import { Container, Logo, LogoutBtn } from '../index';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
-  const authStatus=useSelector((state)=>state.auth.status)
-  const navigate=useNavigate();
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false); // State to manage menu visibility
+
   const navItems = [
-    {
-      name: 'Home',
-      slug: "/",
-      active: true
-    }, 
-    {
-      name: "Login",
-      slug: "/login",
-      active: !authStatus,
-  },
-  {
-      name: "Signup",
-      slug: "/signup",
-      active: !authStatus,
-  },
-  {
-      name: "My Posts",
-      slug: "/my-posts",
-      active: authStatus,
-  },
-  {
-      name: "Add Post",
-      slug: "/add-post",
-      active: authStatus,
-  },
-  ]
+    { name: 'Home', slug: "/", active: true },
+    { name: "Login", slug: "/login", active: !authStatus },
+    { name: "Signup", slug: "/signup", active: !authStatus },
+    { name: "My Posts", slug: "/my-posts", active: authStatus },
+    { name: "Add Post", slug: "/add-post", active: authStatus },
+  ];
 
   return (
-    <header  className='py-3 shadow bg-gray-500'>
-    <Container>
-      <nav className='flex'>
-        <div className='mr-4'>
-          <Link to="/">
-          <Logo width='70px'/>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex items-center justify-between'>
+          <Link to="/" className='flex items-center'>
+            <Logo width='70px'/>
           </Link>
-        </div>
-        
-        <ul className='flex ml-auto'>
-          {navItems.map((item)=>item.active
-          ?( <li key={item.name}>
-              <button onClick={()=>{navigate(item.slug)}}
-              className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>
-                {item.name}
-              </button>
-          </li> ):null)}
+          
+          {/* Hamburger Icon */}
+          <div className='md:hidden'>
+            <button onClick={() => setIsMenuOpen(!isMenuOpen)} className='p-2'>
+              <svg className='w-6 h-6' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'>
+                <path strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='M4 6h16M4 12h16m-7 6h7'></path>
+              </svg>
+            </button>
+          </div>
 
-          {authStatus&&(                          //if authstatus is true code in paranthesis will run
-            <li>
-            <LogoutBtn/>
-            </li>
-          )}
-        </ul>
+          {/* Navigation Items */}
+          <ul className={`md:flex ml-auto ${isMenuOpen ? 'flex' : 'hidden'} flex-col md:flex-row`}>
+            {navItems.map((item) => item.active && (
+              <li key={item.name}>
+                <button onClick={() => navigate(item.slug)} className='block px-6 py-2 rounded-full hover:bg-blue-100 transition-colors'>
+                  {item.name}
+                </button>
+              </li>
+            ))}
 
-
-      </nav>
-    </Container>
+            {authStatus && (
+              <li>
+                <LogoutBtn/>
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
